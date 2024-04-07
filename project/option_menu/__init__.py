1ef35e69c5f644c3b32b69df4806922c062b1d61
Stelios <494002@student.saxion.nl>
2024-04-07 15:19:46 +0200

    test

--- project/option_menu/__init__.py
+++ project/option_menu/__init__.py
@@ -76,23 +76,4 @@ def option_menu(menu_title, options, default_index=0, menu_icon=None, icons=None
 # it is considered a new instance and will be re-mounted on the frontend
 # and lose its current state. In this case, we want to vary the component's
 # "name" argument without having it get recreated.
-if __name__ == "__main__":
-    st.set_page_config(page_title="Option Menu", layout="wide")
-    with st.sidebar:
-        selected = option_menu("Main Menu", ["Home", "Upload","---", "Tasks", 'Settings'], 
-        icons=['house', 'cloud-upload', None, "list-task", 'gear'], menu_icon="cast", default_index=1)
 
-    selected2 = option_menu(None, ["Home", "Upload", "---", "Tasks", 'Settings'], 
-        icons=['house', 'cloud-upload', None, "list-task", 'gear'], 
-        menu_icon="cast", default_index=0, orientation="horizontal")
-
-    selected3 = option_menu(None, ["Home", "Upload",  "Tasks", 'Settings'], 
-        icons=['house', 'cloud-upload', "list-task", 'gear'], 
-        menu_icon="cast", default_index=0, orientation="horizontal",
-        styles={
-            "container": {"padding": "0!important", "background-color": "#fafafa"},
-            "icon": {"color": "orange", "font-size": "25px"}, 
-            "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-            "nav-link-selected": {"background-color": "green"},
-        }
-    )
